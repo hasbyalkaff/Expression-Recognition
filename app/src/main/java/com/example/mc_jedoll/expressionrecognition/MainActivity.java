b53315954d4978b079f398a1401f9f51f8282cb8
hasbyalkaff <hasbyalkaff@gmail.com>
2018-08-30 08:29:45 +0700

    add OpenCV

--- app/src/main/java/com/example/mc_jedoll/expressionrecognition/MainActivity.java
+++ app/src/main/java/com/example/mc_jedoll/expressionrecognition/MainActivity.java
@@ -1,13 +1,134 @@
 package com.example.mc_jedoll.expressionrecognition;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+    private String TAG = "MAIN_TAG";
+    private CameraBridgeViewBase mCamera;
+    private CascadeClassifier mDetection;
+    private int absoluteFaceSize;
+    private Mat grayscaleImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        // inisialisasi kamera
+        mCamera = findViewById(R.id.cameraView);
+        mCamera.setVisibility(SurfaceView.VISIBLE);
+        mCamera.setCvCameraViewListener(this);
+    }
+
+    // Connect to OpenCV
+    private BaseLoaderCallback mLoaderCallback =new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch(status){
+                case LoaderCallbackInterface.SUCCESS:{
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    intializeOpenCVDependencies();
+                } break;
+                default: {
+                    super.onManagerConnected(status);
+                } break;
+            }
+            super.onManagerConnected(status);
+        }
+    };
+
+    private void intializeOpenCVDependencies(){
+        try {
+            InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_alt);
+            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+            File mCascadeFile = new File(cascadeDir, "haarcascade_frontalface_alt.xml");
+            FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1){
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+
+            mDetection = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+            mDetection.load(mCascadeFile.getAbsolutePath());
+            cascadeDir.delete();
+        } catch (Exception e){
+            Log.e(TAG, "Error loading cascade", e);
+        }
+        mCamera.enableView();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mLoaderCallback);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mCamera != null)
+            mCamera.disableView();
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mCamera != null)
+            mCamera.disableView();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        grayscaleImage = new Mat(height, width, CvType.CV_8UC4);
+        absoluteFaceSize = (int)(height*0.2);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Imgproc.cvtColor(inputFrame.rgba(), grayscaleImage, Imgproc.COLOR_RGBA2RGB);
+
+        MatOfRect faces = new MatOfRect();
+        if(mDetection != null){
+            mDetection.detectMultiScale(grayscaleImage, faces, 1.1, 2, 2, new Size(absoluteFaceSize, absoluteFaceSize), new Size());
+        }
+
+        Rect[] facesArray = faces.toArray();
+        for (int i=0; i<facesArray.length; i++){
+            Imgproc.rectangle(inputFrame.gray(),facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 255), 3);
+        }
+        return inputFrame.gray();
     }
 }
